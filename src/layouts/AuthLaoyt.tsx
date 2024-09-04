bdf484e31707812ce79f63e77ee2e9941a91305a
Sohail.Hayat <Sohail.Hayat@cloudsolutions.com.sa>
2024-09-04 17:53:55 +0300

    working on client admin theme

--- src/layouts/AuthLaoyt.tsx
+++ src/layouts/AuthLaoyt.tsx
@@ -1,7 +1,5 @@
 import React from "react";
-import { BrowserRouter, Outlet, Route, Routes } from "react-router-dom";
-import AuthRoutes from "../features/auth/authRoutes";
-import Login from "../features/auth/components/login";
+import {  Outlet} from "react-router-dom";
 
 const backgroundImageStyle = {
     backgroundImage: 'url("https://images.unsplash.com/photo-1497294815431-9365093b7331?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1950&q=80")',
